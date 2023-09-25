3ecc733248dbc8b2d68985c1b9d64c17f74bb3e7
Vinicius Reis <vinireis010305@gmail.com>
2023-09-25 17:40:46 -0300

    Criando formulario com validacao

--- src/app/pages/login-with-jwt/login-with-jwt.component.ts
+++ src/app/pages/login-with-jwt/login-with-jwt.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-with-jwt',
   templateUrl: './login-with-jwt.component.html',
-  styleUrls: ['./login-with-jwt.component.css']
+  styleUrls: ['./login-with-jwt.component.css'],
 })
-export class LoginWithJwtComponent {
+export class LoginWithJwtComponent  {
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+    });
+  }
+  
+  get email(){
+    return this.loginForm.get('email')!;
+  }
 }
