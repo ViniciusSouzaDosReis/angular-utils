d7ff6809b34ae41f700624d97e7b08efd5a4dbad
Vinicius Reis <vinireis010305@gmail.com>
2023-09-25 16:49:18 -0300

    Concertando erro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginWithJwtComponent } from './pages/login-with-jwt/login-with-jwt.component';
 
-const routes: Routes = [
-  {path: 'login', component: }
-];
+const routes: Routes = [{ path: 'login', component: LoginWithJwtComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginWithJwtComponent } from './page/login-with-jwt/login-with-jwt.component';
+import { LoginWithJwtComponent } from './pages/login-with-jwt/login-with-jwt.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginWithJwtComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginWithJwtComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
