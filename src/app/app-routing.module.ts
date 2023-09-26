32ac63b8ce0ad9641d42dfc6481fd7e94c62f8bc
Vinicius Reis <vinireis010305@gmail.com>
2023-09-26 17:04:24 -0300

    Validando se usuario esta logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginWithJwtComponent } from './pages/login-with-jwt/login-with-jwt.component';
 import { HomeComponent } from './pages/home/home.component';
+import { authenticatedUserGuard } from './services/guards/authenticated-user.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginWithJwtComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authenticatedUserGuard],
+  },
 ];
 
 @NgModule({
--- src/app/services/guards/authenticated-user.guard.ts
+++ src/app/services/guards/authenticated-user.guard.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../auth.service';
 export const authenticatedUserGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+  
   if (authService.logged) return true;
   router.navigate(['login']);
   return false;
