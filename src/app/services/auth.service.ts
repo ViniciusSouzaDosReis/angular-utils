144589bdab54dd80a5c0f4fa3cef612fc2a2b47d
Vinicius Reis <vinireis010305@gmail.com>
2023-09-26 08:55:01 -0300

    terminando logica de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -14,7 +16,11 @@ interface User {
 export class AuthService {
   apiUrl = 'https://dev.croma-bas.com:8443/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
 
   login(email: string, password: string): Observable<any> {
     return this.http
@@ -25,7 +31,8 @@ export class AuthService {
       .pipe(
         tap({
           next: (res) => {
-            
+            this.cookieService.set('token', res.access_token);
+            this.router.navigate(['home']);
           },
           error: (err) => {
             if (err.status !== 200) {
